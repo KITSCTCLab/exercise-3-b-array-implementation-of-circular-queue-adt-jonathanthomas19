8384374460d99e1f370980cb476a415c6d5d368d
JONATHAN THOMAS URK21CS2036 <91773936+jonathanthomas19@users.noreply.github.com>
2022-08-10 11:22:02 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,53 +1,31 @@
 class MyCircularQueue:
-    def __init__(self, size: int):
-        # Write code here
-
+    def _init_(self, size: int):
+        self.size=size
+        self.queue=[None]*size
+        self.rear=-1
+        self.front=-1
+    
     def enqueue(self, value: int) -> bool:
-        # Write code here
-
+        if(self.is_full()==False):
+            if(self.front==-1):
+                self.front=0
+                self.rear=0
+                self.queue[self.rear]=value
+            else:
+                self.rear=(self.rear+1)%self.size
+                self.queue[self.rear]=value
+            return True
+        else:
+            return False
+     
     def dequeue(self) -> bool:
-        # Write code here
-
-    def get_front(self) -> int:
-        # Write code here
-
-    def get_rear(self):
-        # Write code here
-
-    def is_empty(self):
-        # Write code here
-
-    def is_full(self):
-        # Write code here
-
-
-# Do not change the following code
-operations = []
-for specific_operation in input().split(','):
-    operations.append(specific_operation.strip())
-data = []
-for item in input().split(','):
-    item = item.strip()
-    if item == '-':
-        data.append([])
-    else:
-        data.append([int(item)])
-obj = MyCircularQueue(data[0][0])
-result = []
-for i in range(len(operations)):
-    if i == 0:
-        result.append(None)
-    elif operations[i] == "enqueue":
-        result.append(obj.enqueue(data[i][0]))
-    elif operations[i] == "get_rear":
-        result.append(obj.get_rear())
-    elif operations[i] == "get_front":
-        result.append(obj.get_front())
-    elif operations[i] == "dequeue":
-        result.append(obj.dequeue())
-    elif operations[i] == "is_full":
-        result.append(obj.is_full())
-    elif operations[i] == "is_empty":
-        result.append(obj.is_empty())
-
-print(result)
+        if(self.is_empty()==False):
+            if(self.front==self.rear):
+                self.front=-1
+                self.rear=-1
+                return True
+            else:
+                self.front=(self.front+1)%self.size
+                return True
+        else:
+            return False
